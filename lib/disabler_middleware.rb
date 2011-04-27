e9e0b895655615a14e904a89c32ad14a4ec203c4
R. Potter <rjspotter@gmail.com>
2011-04-26 22:34:32 -0700

    useful defaults and builder syntax configuration

--- lib/disabler_middleware.rb
+++ lib/disabler_middleware.rb
@@ -3,13 +3,42 @@ module Rack
 
   class Disabler
     
-    def initialize(app, options = {}, &block)
-      @app, @store, @responses = app, options[:store], (options[:responses] || {})
-      @extractor = block || lambda {|env| }
+    def initialize(app,store = nil)
+      @responses,@app,@store = {
+        'freeloader' =>  [402, {}, ["Payment Required"]],
+        'douchebag'  =>  [403, {}, ["Forbidden"]]
+      },app,store
+      yield self if block_given?
+    end
+
+    attr_accessor :store, :responses
+
+    def store
+      @store ||= Redis.new
+    end
+
+    def add_response(key,val)
+      @responses[key.to_s] = val
+    end
+
+    def add_responses(hsh)
+      @responses.merge!(hsh)
+    end
+
+    def response(key)
+      @responses[key]
+    end
+
+    def extractor(&block)
+      @extractor = block
+    end
+
+    def extract(env)
+      (@extractor || lambda {|env| Rack::Request.new(env).path}).call(env)
     end
 
     def call(env)
-      @responses[@store.get(@extractor.call(env))] || @app.call(env)
+      response(store.get(extract(env))) || @app.call(env)
     end
 
   end
--- spec/disabler_middleware_spec.rb
+++ spec/disabler_middleware_spec.rb
@@ -18,10 +18,93 @@ describe "DisablerMiddleware" do
     store.stub(:get)
   end
 
+  context "accessors" do
+    
+    before do
+      @disabler = Rack::Disabler.new(app) do |dis|
+        dis.store = store
+      end
+    end
+
+    it "should have the store be settable" do
+      expect {@disabler.store = "somthing else"}.should_not raise_error
+    end
+
+    it "should retrieve set store" do
+      @disabler.store = foo = "something"
+      @disabler.store.should == foo
+    end
+
+    it "should allow setting of additional responses" do
+      expect {@disabler.responses = {'foo' => 'bar'}}.should_not raise_error
+    end
+
+    it "should make those additional responses available" do
+      @disabler.responses = {'foo' => 'bar'}
+      @disabler.response('foo').should == 'bar'
+    end
+
+    it "should allow the adding of single responses" do
+      @disabler.should respond_to(:add_response)
+    end
+
+    it "should add that single response to the collection" do
+      @disabler.add_response('baz','cool')
+      @disabler.response('baz').should == 'cool'
+    end
+
+    context "defautl responses" do
+      it "should provide a default responses for non-payment" do
+        @disabler.response('freeloader').should == [402, {}, ["Payment Required"]]
+      end
+
+      it "should provide a default responses for abuse" do
+        @disabler.response('douchebag').should == [403, {}, ["Forbidden"]]
+      end
+    end
+
+    it "should be able to bulk add respeonses" do
+      @disabler.add_responses({'white' => 'rabbit', 'red' => 'pill'})
+      %w[white red freeloader].inject(true) {|m,x| !! m && x}.should be_true
+    end
+
+    context "overwriting responses" do
+      
+      it "should work with single" do
+        @disabler.add_response('douchebag',[418,{},"Tip me over and pour me out"])
+        @disabler.response('douchebag').should == [418,{},"Tip me over and pour me out"]
+      end
+
+      it "should work with many" do
+        @disabler.add_responses({'douchebag' => [418,{},"Tip me over and pour me out"]})
+        @disabler.response('douchebag').should == [418,{},"Tip me over and pour me out"]
+      end
+
+    end
+
+    context "extractor" do
+      
+      it "should allow setting of the extractor with a block" do
+        expect {@disabler.extractor {|req| "foo"} }.should_not raise_error
+      end
+
+      it "should make the callable extractor availabe" do
+        @disabler.extractor {|foo| 'bar'}
+        @disabler.extract("something").should == "bar"
+      end
+
+    end
+
+  end
+
   context "base setup" do
     
     before do
-      @disabler = Rack::Disabler.new(app,{:store => store, :responses => {}})
+      @disabler = Rack::Disabler.new(app) do |dis|
+        dis.store = store 
+        dis.responses = {}
+        dis.extractor {|env| "foo"}
+      end
     end
     
     it "should take an app and store it for later" do
@@ -51,12 +134,10 @@ describe "DisablerMiddleware" do
     context "disabled developer" do
 
       before do
-        @disabled = Rack::Disabler.new(app,{
-                                         :store => store, 
-                                         :responses => {
-                                         'freeloader' =>  [402, {}, ['Payment Required']]
-                                         }
-                                       }) {|env| "disableddeveloperkey"}
+        @disabled = Rack::Disabler.new(app) do |dis|
+          dis.store = store
+          dis.extractor {|env| "disableddeveloperkey"}
+        end
       end
 
       it "should call store with disableddeveloperkey" do
@@ -73,7 +154,10 @@ describe "DisablerMiddleware" do
 
     context "allowed developer" do
       before do
-        @allowed = Rack::Disabler.new(app,{:store => store}) {|env| "alloweddeveloperkey"}
+        @allowed = Rack::Disabler.new(app) do |dis|
+          dis.store = store
+          dis.extractor {|env| "alloweddeveloperkey"}
+        end
       end
 
       it "should lookup developer" do
